0e91eea5f7f0c2b90e657a49ec2c8eea4d4aec52
hamzas04 <samraoui.hamza@gmail.com>
2023-03-17 18:30:39 -0700

    2. I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0
  */
 
-int main() {
-    char low;
+int main(void)
+{
+	char low;
 
-    for (low = 'a'; low <= 'z'; low++) {
-        putchar(low);
-    }
-    putchar('\n');
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	        putchar('\n');
 
-    return 0;
+	return (0);
 }
