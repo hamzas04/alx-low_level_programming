053181853f35a3d42f3b849e1b23b9cbe0a428f4
hamzas04 <samraoui.hamza@gmail.com>
2023-03-21 18:49:56 -0700

    Task : 6. There is no such thing as absolute value in this world.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -58,4 +58,13 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: The integer to compute the absolute value of
+ *
+ * Return: The absolute value of n
+ */
+
+int _abs(int n);
+
 #endif
