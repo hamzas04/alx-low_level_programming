b9781958144dd00bfb0c8d119bc875817fcf30ab
hamzas04 <samraoui.hamza@gmail.com>
2023-03-21 18:11:34 -0700

    Task : 3. islower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -30,4 +30,13 @@ void print_alphabet(void);
 
 void print_alphabet_x10(void);
 
+/**
+ * _islower - Checks if a character is lowercase
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c);
+
 #endif
