b62fe8d580cf0afaa905047a9ee6ac3e12264e8b
hamzas04 <samraoui.hamza@gmail.com>
2023-03-21 19:52:08 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,26 +1,20 @@
 #include "main.h"
 
-/**                                                                                                                                            
- * print_last_digit - Prints the last digit of a number                                                                                        
- *                                                                                                                                             
- * @n: The number to get the last digit of                                                                                                     
- *                                                                                                                                             
- * Return: The value of the last digit                                                                                                         
- */
+/**                                                                                                                                             * print_last_digit - Prints the last digit of a number                                                                                        *                                                                                                                                             * @n: The number to get the last digit of                                                                                                     *                                                                                                                                             * Return: The value of the last digit                                                                                                         */
 int print_last_digit(int n)
 {
-        int last_digit;
+		int last_digit;
 
         /* Get the absolute value of the number */
-        if (n < 0)
-                n = -n;
+		if (n < 0)
+				n = -n;
 
         /* Get the last digit */
-        last_digit = n % 10;
+		last_digit = n % 10;
 
         /* Print the last digit */
-        _putchar(last_digit + '0');
+		_putchar(last_digit + '0');
 
         /* Return the value of the last digit */
-        return (last_digit);
+		return (last_digit);
 }
