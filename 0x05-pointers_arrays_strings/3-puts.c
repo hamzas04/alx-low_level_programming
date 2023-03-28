a4e6dcba89b6d1ffbb45b4b16e79560f08ac4f42
hamzas04 <samraoui.hamza@gmail.com>
2023-03-27 17:53:44 -0700

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,8 +11,8 @@ void _puts(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);  // write the character to stdout
+		_putchar(str[i]);
 	}
 
-	_putchar('\n');  // write a newline character to stdout
+	_putchar('\n');
 }
