7fb22291b9f302100da50377b3df33b7994d8f72
hamzas04 <samraoui.hamza@gmail.com>
2023-03-30 17:08:22 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ *
+ * Return: an integer less than, equal to, or greater than zero, depending on
+ * whether s1 is lexicographically less than, equal to, or greater than s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return 0;
+	}
+	else if (*s1 < *s2)
+	{
+		return -1;
+	}
+	else
+	{
+		return 1;
+	}
+}
