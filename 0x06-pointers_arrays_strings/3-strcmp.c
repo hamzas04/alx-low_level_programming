ad144763d6c5e2dde7a97ee5fc4357ee9c37488a
hamzas04 <samraoui.hamza@gmail.com>
2023-03-30 17:10:13 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,14 +17,14 @@ int _strcmp(char *s1, char *s2)
 
 	if (*s1 == *s2)
 	{
-		return 0;
+		return (0);
 	}
 	else if (*s1 < *s2)
 	{
-		return -1;
+		return (-1);
 	}
 	else
 	{
-		return 1;
+		return (1);
 	}
 }
