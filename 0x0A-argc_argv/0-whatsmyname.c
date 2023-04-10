ad1e4fa773a9cd2b2519c850d70cd7c96fa79d79
hamzas04 <samraoui.hamza@gmail.com>
2023-04-10 16:49:34 -0700

    0. It aint what they call you, its what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of pointers to the arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	_putchar("%s\n", argv[0]);
+	return (0);
+}
