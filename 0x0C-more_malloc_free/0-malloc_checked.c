3eadc9252c08d97f122d29f89a1d62217337cbe9
hamzas04 <samraoui.hamza@gmail.com>
2023-04-13 16:38:48 -0700

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,7 @@
  * @b: amount of memory to allocate
  *
  * Return: pointer to the allocated memory
- * If malloc fails, the function causes normal 
+ * If malloc fails, the function causes normal
  * status value of 98.
  */
 void *malloc_checked(unsigned int b)
