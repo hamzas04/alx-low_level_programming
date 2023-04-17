36f75872408bce540816c566f0c1c93222aa829b
hamzas04 <samraoui.hamza@gmail.com>
2023-04-17 16:54:20 -0700

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,7 +8,7 @@
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: pointer to the new dog object, or NULL if malloc fails
+ * Return: pointer to the new dog object, or NULL
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
