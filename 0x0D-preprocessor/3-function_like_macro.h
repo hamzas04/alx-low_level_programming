462f00996a9e2ca5cfebb41596248a38b9f6e156
hamzas04 <samraoui.hamza@gmail.com>
2023-04-17 11:24:47 -0700

    3. Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,15 +1,10 @@
-#include <stdio.h>
-
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 /**
  * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
-	int x = -10;
-	int abs_x = ABS(x);
-	printf("%d, %d\n", x, abs_x);
-	return 0;
-}
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif
