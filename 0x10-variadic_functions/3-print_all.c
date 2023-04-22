79e6dd956d1db1c0af30f5f69b64b96556ab0bfb
hamzas04 <samraoui.hamza@gmail.com>
2023-04-21 19:49:22 -0700

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,62 +1,53 @@
 #include "variadic_functions.h"
 
-/**
- * print_all - prints anything.
- * @format: list of types of arguments passed to the function.
- * c: char
- * i: integer
- * f: float
- * s: char * (if the string is NULL, print (nil) instead
- * any other char should be ignored.
- * Return: void.
- */
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
 
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: void
+ */
 void print_all(const char * const format, ...)
 {
 	va_list args;
+	const char *c;
+	char *s;
+
 	va_start(args, format);
 
-	const char *c;
-	int i;
-	float f;
-	char *s;
-	int printed = 0;
-
-	while ((c = format++) != '\0')
+	c = format;
+	while (c && *c)
 	{
 		switch (*c)
 		{
 			case 'c':
 				printf("%c", va_arg(args, int));
-				printed = 1;
 				break;
 			case 'i':
 				printf("%d", va_arg(args, int));
-				printed = 1;
 				break;
 			case 'f':
 				printf("%f", va_arg(args, double));
-				printed = 1;
 				break;
 			case 's':
 				s = va_arg(args, char *);
 				if (s == NULL)
-				{
-					printf("(nil)");
-				}
-				else
-				{
-					printf("%s", s);
-				}
-				printed = 1;
+					s = "(nil)";
+				printf("%s", s);
 				break;
 			default:
-				break;
+				c++;
+				continue;
 		}
-		if (*(c + 1) && printed)
+
+		if (*(++c))
 			printf(", ");
-		printed = 0;
+
 	}
+
 	printf("\n");
 	va_end(args);
 }
