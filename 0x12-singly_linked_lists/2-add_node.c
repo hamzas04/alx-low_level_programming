a0716e1966fe1feda06e3ea2739a68647dbba79e
hamzas04 <samraoui.hamza@gmail.com>
2023-04-27 18:23:33 -0700

    2. Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,6 +10,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int len = 0;
 
 	if (str == NULL)
 		return (NULL);
@@ -18,15 +19,13 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+	while (str[len])
+		len++;
 
-	new_node->len = strlen(str);
+	new_node->str = strdup(str);
+	new_node->len = len;
 	new_node->next = *head;
+
 	*head = new_node;
 
 	return (new_node);
