13e25f11465d924faacbea2bec141e0834bfcf17
hamzas04 <samraoui.hamza@gmail.com>
2023-05-02 17:18:15 -0700

    1. List length

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -7,12 +7,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-} listint_t;
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 #endif /* LISTS_H */
